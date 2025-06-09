1caac958bc1ba3140c7262b37cb5afba61f7db29
Hamed Abasi <ha.abasi@gmail.com>
2025-06-10 00:29:30 +0330

    broadcast message to all other clients

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
     ws.send('Hello, I am the server and I received your message: ' + message);
+
+    // We can broadcast the message to all connected clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send("somebody said: " + message);
+      }
+    });
+
   });
 
   ws.send('Connection established');
