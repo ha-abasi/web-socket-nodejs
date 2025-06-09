b32069e6d8f84cf873f70001660b8af2fd0bf7f9
Hamed Abasi <ha.abasi@gmail.com>
2025-06-10 00:24:05 +0330

    server & client

---
+++ server.js
@@ -0,0 +1,13 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send('Hello, I am the server and I received your message: ' + message);
+  });
+
+  ws.send('Connection established');
+});
+
+console.log('WebSocket server is running on port 8080');
